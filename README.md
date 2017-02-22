de88c7f12609aa6d2d8ce5e93f9a18bf6259bcf8
huangpuyuan <huangpuyuan@outlook.com>
2017-02-23 00:57:27 +0800

    filters

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 this repo is  FOR using AngularJS。Also using the Nodejs,Git,NPM。and so on..
 
 JS Minification 最小化。 https://javascript-minifier.com/
+
+
+lecture 13:
+
+STEPS to create a custom filter
+	1.Define filter factory function
+	2.Register filter factory function with module
+To Use custom filter in JavaScript
+	1.inject filter function registered*NameFilter* into controller
+To Use in HTML -- no need to inject into controller
+	1.{{ expression | registeredName  }}
---
+++ fullstack-course/examples/filter/app.js
@@ -0,0 +1,26 @@
+(function() {
+    "use strict";
+
+    angular.module('MsgApp', [])
+        .controller('MsgController', MsgController);
+
+    MsgController.$inject = ['$scope', '$filter'];
+
+    function MsgController($scope,$filter) {
+        $scope.name = "Yaakov";
+        $scope.stateOfBeing = "hungry";
+        $scope.cookieCost = .45;
+
+        $scope.sayMessage = function() {
+        	var msg = "Yaakov liske to eat healthy snacks at night!";
+        	var output = $filter('uppercase')(msg);
+            return output;
+        }
+
+        $scope.feedYaakov = function() {
+            $scope.stateOfBeing = "fed";
+        }
+    };
+
+
+})()
